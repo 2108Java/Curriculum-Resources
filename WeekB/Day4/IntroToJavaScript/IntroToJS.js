8e3b5ef87972e831e1f5d1e0acde83119945f88a
BenArayathel <ben.arayathel@gmail.com>
2021-09-09 15:34:20 -0400

    DOM Manipulation

--- WeekB/Day4/IntroToJavaScript/IntroToJS.js
+++ WeekB/Day4/IntroToJavaScript/IntroToJS.js
@@ -104,6 +104,55 @@ function titleToGreen(){
     title.setAttribute("style","color:green");
 }
 
+function setTable(){
+    var fruit1 = {"name":"kiwi","price":2};
+var fruit2 = {"name":"apple", "price": 2};
+var fruit3 = {"name":"tomato", "price": 3};
+var fruit4 = {"name":"banana", "price": 4};
+var fruit5 = {"name":"dragonfruit", "price": 1};
+
+fruit1.tasty = true;
+
+//JS vs Java?
+// Arrays are mutable in size 
+// We can have different datatypes 
+var fruitArray = []; //creating an array 
+
+fruitArray[0] = fruit1;
+fruitArray[1] = fruit2;
+fruitArray[2] = fruit3;
+fruitArray[3] = fruit4;
+fruitArray[4] = fruit5;
+
+
+for(var i = 0; i < fruitArray.length; i++){
+
+    addRow(fruitArray[i].name, fruitArray[i].price);
+}
+
+}
+
+function addRow(textValue, priceText){
+    let table = document.getElementById("fruit-table");
+
+    //Create elements
+    
+    //Creating a column
+    let tableRow = document.createElement("tr");
+    let nameColumn = document.createElement("td");
+    let priceColumn = document.createElement("td");
+
+    //Attaching those elements to the the table row
+    tableRow.appendChild(nameColumn);
+    tableRow.appendChild(priceColumn);
+
+    //Attaching the table row to the table
+    table.appendChild(tableRow);
+    
+    //Assigning text inside of those columns
+    nameColumn.innerText = textValue;
+    priceColumn.innerText = priceText;
+}
 
 //Creating a dynamic webpage 
 
@@ -113,7 +162,46 @@ window.onload = function(){ //as soon as the page is loaded
     
     var greenButton = document.getElementById("green");
 
+    var tableButton = document.getElementById("tableButton");
+
+    var createTableButton = document.getElementById("createTable");
+
+    tableButton.addEventListener('click', addRow);
+
     redButton.addEventListener('click', titleToRed);
     greenButton.addEventListener('click',titleToGreen);
+
+    createTableButton.addEventListener('click',setTable);
     
 }
+
+
+//Objects and array in JS
+
+//Objects in JS are key value pairs;
+//There's no "new" keyword, we don't need a constructor to "create" an object 
+//We don't need a class eitehr in JS 
+
+
+var fruit1 = {"name":"kiwi","price":2};
+var fruit2 = {"name":"apple"};
+var fruit3 = {"name":"tomato"};
+var fruit4 = {"name":"banana"};
+var fruit5 = {"name":"dragonfruit"};
+
+fruit1.tasty = true;
+
+//JS vs Java?
+// Arrays are mutable in size 
+// We can have different datatypes 
+var fruitArray = []; //creating an array 
+
+fruitArray[0] = fruit1;
+fruitArray[1] = fruit2;
+fruitArray[2] = fruit3;
+fruitArray[3] = fruit4;
+fruitArray[4] = fruit5;
+
+console.log(fruitArray);
+
+console.log(fruit1);
