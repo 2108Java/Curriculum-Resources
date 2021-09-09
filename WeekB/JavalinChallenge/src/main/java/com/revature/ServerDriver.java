66e3433625a335bce5c8dcfa446200422742955b
BenArayathel <ben.arayathel@gmail.com>
2021-09-09 17:02:52 -0400

    full stack ajax example

--- WeekB/JavalinChallenge/src/main/java/com/revature/ServerDriver.java
+++ WeekB/JavalinChallenge/src/main/java/com/revature/ServerDriver.java
@@ -1,5 +1,6 @@
 package com.revature;
 
+import java.io.ObjectInputFilter.Config;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,9 +11,25 @@ import io.javalin.http.Context;
 
 public class ServerDriver {
 	
-	List<Planet> planetList = new ArrayList<>();
+	private static List<Planet> planetList = new ArrayList<>();
 	
 	public static String createPlanet(Context ctx) {
+		System.out.println(ctx.queryParam("name"));
+		System.out.println(ctx.queryParam("mass"));
+		System.out.println(ctx.queryParam("rings"));
+		
+		Planet planet;
+		if(ctx.queryParam("rings") == null) {
+			planet = new Planet(ctx.queryParam("name"), 
+					Double.parseDouble(ctx.queryParam("mass")), 
+					false);
+		}else {
+			planet = new Planet(ctx.queryParam("name"), 
+					Double.parseDouble(ctx.queryParam("mass")), 
+					true);
+		}
+		
+		planetList.add(planet);
 		return "create";
 	}
 	
@@ -20,8 +37,10 @@ public class ServerDriver {
 		return "delete";
 		}
 		
-	public static String seeAllPlanets(Context ctx) {
-		return "see all planets";
+	public static List<Planet> seeAllPlanets(Context ctx) {
+		
+		
+		return planetList;
 	}
 	
 	public static String updatePlanetsMass(Context ctx) {
@@ -31,13 +50,23 @@ public class ServerDriver {
 	public static void main(String[] args) {
 
 
-		Javalin app = Javalin.create().start(9000);
+		Javalin app = Javalin.create(
+				config -> 
+				{
+					config.addStaticFiles("/");
+				}
+				).start(9000);
+		
+		planetList.add(new Planet("Mercury", 0.2, false));
+		planetList.add(new Planet("Venus", 0.8, false));
+		planetList.add(new Planet("Earth", 1, false));
 		
 		
 		//Setting up endpoints
 		app.get("/create", ctx -> ctx.html(createPlanet(ctx)));
 		app.get("/delete", ctx -> ctx.html(deletePlanet(ctx)));
-		app.get("/view-all", ctx -> ctx.html(seeAllPlanets(ctx)));
+		//JSON = Javascript Object Notation
+		app.get("/view-all", ctx -> ctx.json(seeAllPlanets(ctx)));
 		app.get("/update", ctx -> ctx.html(updatePlanetsMass(ctx)));
 
 	}
